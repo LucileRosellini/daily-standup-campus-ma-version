2eb6dc8fafdd7f1d39df0e3b15723e8fdd160f7b
Lucile ROSELLINI <l.rosellini@cerberis.com>
2020-10-20 15:08:24 +0200

    securite

--- src/app/projets.service.ts
+++ src/app/projets.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+/*import { HttpHeaders } from '@angular/common/http';*/
+
+import { Observable, of } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+import { Projet } from './projet';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ProjetsService {
+export class configService {
 
-  constructor() { }
+  projetUrlApi = 'https://daily-standup-campus.herokuapp.com/api/projects';
+
+
+
+  constructor(private http: HttpClient) { }
+
 }
